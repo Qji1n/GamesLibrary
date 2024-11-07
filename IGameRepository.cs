170c83b1f1be8e72585052e5204bfbf005454e5f
Qji1n <vit04rom@mail.ru>
2024-11-07 14:40:13 +0500

    Update

--- IGameRepository.cs
+++ IGameRepository.cs
@@ -3,7 +3,7 @@
     public interface IGameRepository
     {
         Task<Game> AddGameAsync(Game game);
-        Task<IEnumerable<Game>> GetAllGamesAsync(string genre = null);
+        Task<IEnumerable<Game>> GetAllGamesAsync(string type = null);
         Task<Game> UpdateGameAsync(Game game);
         Task<bool> DeleteGameAsync(int id);
     }
